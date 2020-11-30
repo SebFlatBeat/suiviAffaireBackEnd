8a822e01ec3be9fac865b34b6f901458ce83e29c
SebFlatBeat <sebastien.darre@gmail.com>
2020-11-30 20:52:29 +0100

    Implementation de l'injection des données SGE / GEC / SGO

---
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/utils/Data.java
@@ -0,0 +1,39 @@
+package fr.enedis.cliffs.qdd.suiviaffairebackend.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Data {
+    @Value("${inputGECFile}")
+    String inputGECFile;
+
+    @Value("${inputSGEFile}")
+    String inputSGEFile;
+
+    @Value("${inputSGOFile}")
+    String inputSGOFile;
+
+    @Autowired
+    Reader reader;
+
+    public void parsing(){
+        try {
+            reader.readFileSGE(inputSGEFile);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            reader.readFileGEC(inputGECFile);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            reader.readFileSGO(inputSGOFile);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
