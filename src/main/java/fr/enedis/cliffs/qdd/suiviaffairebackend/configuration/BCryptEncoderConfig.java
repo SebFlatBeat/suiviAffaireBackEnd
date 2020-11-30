50042367bc5da41e93712c2bf9724f593a24d3e9
SebFlatBeat <sebastien.darre@gmail.com>
2020-11-30 15:24:27 +0100

    Déplacement de BCryptEncoderConfig dans le package config

--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/configuration/BCryptEncoderConfig.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/configuration/BCryptEncoderConfig.java
@@ -1,4 +1,4 @@
-package fr.enedis.cliffs.qdd.suiviaffairebackend.security;
+package fr.enedis.cliffs.qdd.suiviaffairebackend.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
