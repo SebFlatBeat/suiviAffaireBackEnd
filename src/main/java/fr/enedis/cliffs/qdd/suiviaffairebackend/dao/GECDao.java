79e655d8061907d26149d8270598bf9787b3986b
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-04 00:27:14 +0100

    implementation de findAll()

--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/dao/GECDao.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/dao/GECDao.java
@@ -3,7 +3,12 @@ package fr.enedis.cliffs.qdd.suiviaffairebackend.dao;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.GEC;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GECDao extends JpaRepository<GEC,Long> {
 
     GEC save(GEC gec);
+
+
+    List<GEC> findAll();
 }
--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/dao/SGODao.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/dao/SGODao.java
@@ -3,7 +3,11 @@ package fr.enedis.cliffs.qdd.suiviaffairebackend.dao;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.SGO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SGODao extends JpaRepository<SGO, Long> {
 
     SGO save(SGO sgo);
+
+    List<SGO>findAll();
 }
