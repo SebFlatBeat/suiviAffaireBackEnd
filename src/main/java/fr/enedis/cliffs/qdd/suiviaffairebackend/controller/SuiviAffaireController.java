276e074dc580c40c4ca9f3b077f6f45774d809d0
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-04 15:34:30 +0100

    Réimplementation du controller

--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/SuiviAffaireController.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/SuiviAffaireController.java
@@ -1,13 +1,13 @@
 package fr.enedis.cliffs.qdd.suiviaffairebackend.controller;
 
-import fr.enedis.cliffs.qdd.suiviaffairebackend.beans.AllDataListBean;
+import fr.enedis.cliffs.qdd.suiviaffairebackend.dto.AllDataListDto;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.GEC;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.SGE;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.SGO;
+import fr.enedis.cliffs.qdd.suiviaffairebackend.service.AllDataService;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.service.GECService;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.service.SGEService;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.service.SGOService;
-import fr.enedis.cliffs.qdd.suiviaffairebackend.utils.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +18,6 @@ import java.util.List;
 public class SuiviAffaireController {
 
     @Autowired
-    Data data;
-
-    @Autowired
     SGEService sgeService;
 
     @Autowired
@@ -29,20 +26,11 @@ public class SuiviAffaireController {
     @Autowired
     GECService gecService;
 
-    @GetMapping("getFiles")
-    public void getFiles(){
-        data.parsing();
-    }
+    @Autowired
+    AllDataService allDataService;
 
     @GetMapping("synthese")
-    public AllDataListBean allData(){
-        List <SGE> sgeList = sgeService.findAll();
-        List <SGO> sgoList = sgoService.findAll();
-        List <GEC> gecList = gecService.findAll();
-        AllDataListBean allDataListBean = new AllDataListBean();
-        allDataListBean.setSge(sgeList);
-        allDataListBean.setSgo(sgoList);
-        allDataListBean.setGec(gecList);
-        return allDataListBean;
+    public AllDataListDto getAllData(){
+        return allDataService.findAllData();
     }
 }
