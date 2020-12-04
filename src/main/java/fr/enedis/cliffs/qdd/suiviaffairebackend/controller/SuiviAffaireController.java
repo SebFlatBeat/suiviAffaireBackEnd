1b291a010e199877301453d7714c430d50b272e2
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-04 17:17:14 +0100

    Code smells

--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/SuiviAffaireController.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/SuiviAffaireController.java
@@ -1,30 +1,15 @@
 package fr.enedis.cliffs.qdd.suiviaffairebackend.controller;
 
 import fr.enedis.cliffs.qdd.suiviaffairebackend.dto.AllDataListDto;
-import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.GEC;
-import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.SGE;
-import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.SGO;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.service.AllDataService;
-import fr.enedis.cliffs.qdd.suiviaffairebackend.service.GECService;
-import fr.enedis.cliffs.qdd.suiviaffairebackend.service.SGEService;
-import fr.enedis.cliffs.qdd.suiviaffairebackend.service.SGOService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class SuiviAffaireController {
 
-    @Autowired
-    SGEService sgeService;
-
-    @Autowired
-    SGOService sgoService;
-
-    @Autowired
-    GECService gecService;
 
     @Autowired
     AllDataService allDataService;
