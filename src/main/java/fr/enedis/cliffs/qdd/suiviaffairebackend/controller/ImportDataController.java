66f82c7a85a5a0dc0f357477dfb791c47a3fcdba
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-04 15:30:10 +0100

    Creation d'un controller pour  recevoir les fichier sources

---
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/ImportDataController.java
@@ -0,0 +1,18 @@
+package fr.enedis.cliffs.qdd.suiviaffairebackend.controller;
+
+import fr.enedis.cliffs.qdd.suiviaffairebackend.utils.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImportDataController {
+
+    @Autowired
+    Data data;
+
+    @GetMapping("getFiles")
+    public void getFiles(){
+        data.parsing();
+    }
+}
