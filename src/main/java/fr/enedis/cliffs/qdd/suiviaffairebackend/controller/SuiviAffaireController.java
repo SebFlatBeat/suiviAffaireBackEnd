d4d482b8f950aa807bed0672cb2a4711ccb2fbdb
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-08 17:11:11 +0100

    Ajout d'analyse dans permit all et ajout du @CrossOrigin

--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/SuiviAffaireController.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/SuiviAffaireController.java
@@ -3,10 +3,11 @@ package fr.enedis.cliffs.qdd.suiviaffairebackend.controller;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.dto.AllDataListDto;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.service.AllDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class SuiviAffaireController {
 
