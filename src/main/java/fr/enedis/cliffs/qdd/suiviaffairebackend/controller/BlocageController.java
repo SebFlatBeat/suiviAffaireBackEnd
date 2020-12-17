162cd3d69896786ed4a65b25548e35ffe68c223a
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-17 01:31:32 +0100

    Suppression codeSmells

--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/BlocageController.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/BlocageController.java
@@ -3,8 +3,6 @@ package fr.enedis.cliffs.qdd.suiviaffairebackend.controller;
 
 import fr.enedis.cliffs.qdd.suiviaffairebackend.service.BlocageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
