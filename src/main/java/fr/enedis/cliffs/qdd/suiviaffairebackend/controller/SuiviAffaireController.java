c4ffff21909b5a57ae6b8331df62fcdefc92444f
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-22 01:48:00 +0100

    implementation de la requete user

--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/SuiviAffaireController.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/controller/SuiviAffaireController.java
@@ -3,15 +3,17 @@ package fr.enedis.cliffs.qdd.suiviaffairebackend.controller;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.dto.FilterForm;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.Blocage;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.SGE;
+import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.UserApp;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.service.BlocageService;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.service.SGEService;
+import fr.enedis.cliffs.qdd.suiviaffairebackend.service.UserAppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -23,6 +25,9 @@ public class SuiviAffaireController {
 
     @Autowired
     BlocageService blocageService;
+
+    @Autowired
+    UserAppService userAppService;
 
     @GetMapping("analyse")
     public Page<Blocage> blocagePagined() {
@@ -39,5 +44,10 @@ public class SuiviAffaireController {
     @GetMapping("synthese")
     public int[] blocagesPercent() {
         return blocageService.percent();
+    }
+
+    @GetMapping("user")
+    public Optional<UserApp> userApp(@RequestParam String userApp){
+        return userAppService.findByUsername(userApp);
     }
 }
