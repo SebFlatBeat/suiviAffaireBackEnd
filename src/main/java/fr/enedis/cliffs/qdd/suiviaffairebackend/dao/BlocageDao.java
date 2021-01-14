1f2839b4f741da661cce509c5384c97c40333de8
SebFlatBeat <sebastien.darre@gmail.com>
2021-01-14 20:50:24 +0100

    code smells

--- src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/dao/BlocageDao.java
+++ src/main/java/fr/enedis/cliffs/qdd/suiviaffairebackend/dao/BlocageDao.java
@@ -4,7 +4,6 @@ import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.Blocage;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.BlocageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
