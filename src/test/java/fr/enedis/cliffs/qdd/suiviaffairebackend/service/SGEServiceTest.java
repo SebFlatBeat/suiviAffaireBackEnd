2c8ae6a34be7e3288b2772f28b465ccac38c9c72
SebFlatBeat <sebastien.darre@gmail.com>
2021-01-19 09:12:57 +0100

    Implementation de SGEServiceTest

--- src/test/java/fr/enedis/cliffs/qdd/suiviaffairebackend/service/SGEServiceTest.java
+++ src/test/java/fr/enedis/cliffs/qdd/suiviaffairebackend/service/SGEServiceTest.java
@@ -6,80 +6,62 @@ import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.GEC;
 import fr.enedis.cliffs.qdd.suiviaffairebackend.entities.SGE;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
 
 class SGEServiceTest {
 
     private SGEService sgeServiceUnderTest;
 
+    private List<SGE> sgeTest = new ArrayList<>();
+
+    private Page<SGE> sgePageTest = new PageImpl<SGE>(sgeTest);
+
     @BeforeEach
     void setUp() {
         sgeServiceUnderTest = new SGEService();
         sgeServiceUnderTest.sgeDao = mock(SGEDao.class);
-    }
 
-    @Test
-    void testSaveSGE() {
-        // Setup
-        final SGE sge = new SGE(0L, "numeroAffaire", 0L, 0L, "portefeuille", "prestation", "contratDemande", "contratInitial", new COSY(0L, "numeroAffaire", "etatAffaire", "intervention"), new GEC(0L, 0L, "etatContractuel", "modeReleve", 0L, "statutDt", "prestationRealise", "realisation"));
-
-        // Configure SGEDao.save(...).
-        final SGE sge1 = new SGE(0L, "numeroAffaire", 0L, 0L, "portefeuille", "prestation", "contratDemande", "contratInitial", new COSY(0L, "numeroAffaire", "etatAffaire", "intervention"), new GEC(0L, 0L, "etatContractuel", "modeReleve", 0L, "statutDt", "prestationRealise", "realisation"));
-        when(sgeServiceUnderTest.sgeDao.save(any(SGE.class))).thenReturn(sge1);
-
-        // Run the test
-        sgeServiceUnderTest.saveSGE(sge);
-
-        // Verify the results
-    }
-
-    @Test
-    void testSaveSGE_SGEDaoReturnsNull() {
-        // Setup
-        final SGE sge = new SGE(0L, "numeroAffaire", 0L, 0L, "portefeuille", "prestation", "contratDemande", "contratInitial", new COSY(0L, "numeroAffaire", "etatAffaire", "intervention"), new GEC(0L, 0L, "etatContractuel", "modeReleve", 0L, "statutDt", "prestationRealise", "realisation"));
-        when(sgeServiceUnderTest.sgeDao.save(any(SGE.class))).thenReturn(null);
-
-        // Run the test
-        sgeServiceUnderTest.saveSGE(sge);
-
-        // Verify the results
+        SGE sge = new SGE(0L, "numeroAffaire", 0L, 0L, "portefeuille", "prestation", "contratDemande", "contratInitial", new COSY(0L, "numeroAffaire", "etatAffaire", "intervention"), new GEC(0L, 0L, "etatContractuel", "modeReleve", 0L, "statutDt", "prestationRealise", "realisation"));
+        sgeTest.add(sge);
+        SGE sge1 = new SGE(1L, "numeroAffaire1", 1L, 1L, "portefeuille1", "prestation1", "contratDemande1", "contratInitial1", new COSY(1L, "numeroAffaire1", "etatAffaire1", "intervention1"), new GEC(1L, 1L, "etatContractuel1", "modeReleve1", 1L, "statutDt1", "prestationRealise1", "realisation1"));
+        sgeTest.add(sge1);
+        Mockito.when(sgeServiceUnderTest.sgeDao.findAll()).thenReturn(sgeTest);
+        Mockito.when(sgeServiceUnderTest.sgeDao.findAll(any(Pageable.class))).thenReturn(sgePageTest);
     }
 
     @Test
     void testFindAll() {
-        // Setup
-
         // Configure SGEDao.findAll(...).
-        final List<SGE> sges = Arrays.asList(new SGE(0L, "numeroAffaire", 0L, 0L, "portefeuille", "prestation", "contratDemande", "contratInitial", new COSY(0L, "numeroAffaire", "etatAffaire", "intervention"), new GEC(0L, 0L, "etatContractuel", "modeReleve", 0L, "statutDt", "prestationRealise", "realisation")));
-        when(sgeServiceUnderTest.sgeDao.findAll()).thenReturn(sges);
+        final List<SGE> sges = sgeTest;
 
         // Run the test
         final List<SGE> result = sgeServiceUnderTest.findAll();
 
         // Verify the results
+        assertEquals(sges, result);
     }
 
     @Test
     void testFindAllPageable() {
-        // Setup
-
         // Configure SGEDao.findAll(...).
-        final Page<SGE> sges = new PageImpl<>(Arrays.asList(new SGE(0L, "numeroAffaire", 0L, 0L, "portefeuille", "prestation", "contratDemande", "contratInitial", new COSY(0L, "numeroAffaire", "etatAffaire", "intervention"), new GEC(0L, 0L, "etatContractuel", "modeReleve", 0L, "statutDt", "prestationRealise", "realisation"))));
-        when(sgeServiceUnderTest.sgeDao.findAll(any(Pageable.class))).thenReturn(sges);
+        final Page<SGE> sges = sgePageTest;
 
         // Run the test
         final Page<SGE> result = sgeServiceUnderTest.findAllPageable(PageRequest.of(0, 1));
 
         // Verify the results
+        assertEquals(sges, result);
     }
 }
