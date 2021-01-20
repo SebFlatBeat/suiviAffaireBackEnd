20036249d35b05afa5394de95f2439899a9481a2
SebFlatBeat <sebastien.darre@gmail.com>
2021-01-20 12:10:39 +0100

    Code Smells

--- src/test/java/fr/enedis/cliffs/qdd/suiviaffairebackend/service/COSYServiceTest.java
+++ src/test/java/fr/enedis/cliffs/qdd/suiviaffairebackend/service/COSYServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
